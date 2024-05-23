dbe09515b13ea2c3e2e7871bbeeda743f84d5c69
Asmita Kumari <asmitababhanlai@gmail.com>
2024-05-23 18:14:23 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Weather = () => {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [error, setError] = useState("");
+  const API_KEY = "43e5dc431fadb6537c1240baed596e25";
+
+  const getWeather = async () => {
+    try {
+      if (!city.trim()) {
+        setError("city name cannot be empty");
+        return;
+      }
+      setError("");
+      let response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      const temp = await response.json();
+      setWeather(temp);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  console.log(weather);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="enter city name"
+      />
+      <button onClick={getWeather}>Get Weather</button>
+      <div>
+        {error && <label style={{ color: "red" }}>{error}</label>}
+        {weather && (
+          <div>
+            <h3>{weather.name}</h3>
+            <p>Temperature: {weather.main.temp}°C</p>
+            <p>Weather: {weather.weather[0].description}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Weather;
